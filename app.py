7b378ea1fb780edc61a7141c8e77ae70ef3cb526
Janov <achillepoirier01@gmail.com>
2025-03-12 11:13:35 +0100

    ADD multiple routes for each case

--- app.py
+++ app.py
@@ -1,35 +1,53 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clicks.db'
 db = SQLAlchemy(app)
-CORS(app)
 
+# Update the model to include a name field
 class Clicks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)  # Name of the action (student_click, etc.)
     count = db.Column(db.Integer, default=0)
 
+# Create tables if they don't exist
 with app.app_context():
     db.create_all()
 
-@app.route('/click', methods=['POST'])
-def increment_click():
-    click_data = Clicks.query.first()
-    if not click_data:
-        click_data = Clicks(count=0)
-        db.session.add(click_data)
+@app.route('/student_click', methods=['POST'])
+def student_click():
+    return increment_click('student_click')
 
-    click_data.count += 1
-    db.session.commit()
-    return jsonify({'count': click_data.count})
+@app.route('/student_write', methods=['POST'])
+def student_write():
+    return increment_click('student_write')
+
+@app.route('/staff_click', methods=['POST'])
+def staff_click():
+    return increment_click('staff_click')
+
+@app.route('/staff_write', methods=['POST'])
+def staff_write():
+    return increment_click('staff_write')
 
 @app.route('/clicks', methods=['GET'])
 def get_clicks():
-    click_data = Clicks.query.first()
-    return jsonify({'count': click_data.count if click_data else 0})
+    all_clicks = Clicks.query.all()
+    clicks_dict = {click.name: click.count for click in all_clicks}
+    return jsonify(clicks_dict)
+
+def increment_click(name):
+    # Find the row with the corresponding name (route)
+    click_data = Clicks.query.filter_by(name=name).first()
+    if not click_data:
+        click_data = Clicks(name=name, count=0)  # Create a new row if not found
+        db.session.add(click_data)
+
+    click_data.count += 1  # Increment the count
+    db.session.commit()
+
+    return jsonify({name: click_data.count})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-
